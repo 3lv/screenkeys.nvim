78140c205eb0e8267991f3e9c9b3050cee904891
3lv <cristian.vlad.vladutu@gmail.com>
2023-08-29 21:51:54 +0300

    working
    code is messy

--- lua/screenkeys.lua
+++ lua/screenkeys.lua
@@ -1,17 +1,12 @@
-local M = {
-	width = 20,
-	height = 2,
-
-}
-local config = {
-	widht = 20,
-	height = 2,
+M = {
+	width = 29,
+	height = 1,
 }
 
 function M.create_buf()
 	M.buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_option(M.buf, "filetype", "screenkeys")
 end
-
 function M.open_win()
 	if M.win ~= nil and vim.api.nvim_win_is_valid(M.win) then
 		return
@@ -24,14 +19,15 @@ function M.open_win()
 		style = "minimal",
 		focusable = false,
 		relative = "editor",
-		anchor = "NE",
+		anchor = "SE",
 		zindex = 300,
 		row = 0,
-		col = vim.o.columns,
+		col = vim.o.columns + 1e9,
 		width = M.width,
 		height = M.height,
 	}
 	M.win = vim.api.nvim_open_win(M.buf, false, options)
+	vim.api.nvim_win_set_option(M.win,  "winhighlight", "NormalFloat:ScreenKeys")
 	-- nivm_win_set_hl_ns(M.win, {ns_id})
 end
 function M.close_win()
@@ -40,91 +36,75 @@ function M.close_win()
 	end
 end
 
-function M.buf_last_line()
-	return vim.api.nvim_buf_line_count(M.buf) - 1;
-end
 
-function M.buf_last_col()
-	local first_line = vim.api.nvim_buf_get_lines(M.buf, -2, -1, 0)[1]
-	if type(first_line) ~= "string" then
-		return -1
-	else
-		return string.len(first_line) - 1
-	end
-end
-
-function M.append_buf(text)
-	--vim.api.nvim_buf_get_lines(13, -2, -1, 0)
-	local last_line = M.buf_last_line();
-	local last_col = M.buf_last_col();
-	vim.api.nvim_buf_set_text(M.buf,
-		last_line, last_col + 1, last_line, last_col + 1,
-		{ text }
-		)
-	-- last character is not visible
-	if vim.fn.screenpos(M.win, 1, 1000).row == 0 then
-		-- delete first visual line from buffer
-		local last_vcol = vim.fn.virtcol2col(M.win, 1, M.width) - 1
-		vim.api.nvim_buf_set_text(M.buf,
-			0, 0, 0, last_vcol + 1,
-			{ "" }
-			)
-	end
-end
-function M.clear_buf()
-	vim.api.nvim_buf_set_lines(M.buf,
-		0, -1, true,
-		{ "" }
-		)
-end
-
--- SpecialKey
 M.cazan = {
-	["\n"] = "^J",
-	["\t"] = "^I",
-	["\x0d"] = "<cr>",
+	["<nl>"] = "<c-j>",
+	["<tab>"] = "↔",
+	["<cr>"] = "⏎",
+	["<space>"] = "␣",
 	["scl"] = "s",
-	["\x80kb"] = "<bs>",
-	["\x80ku"] = "<up>",
-	["\x80kd"] = "<down>",
-	["\x80kr"] = "<right>",
-	["\x80kl"] = "<left>",
+	["<bs>"] = "⌫",
+	["<up>"] = "↑",
+	["<down>"] = "↓",
+	["<right>"] = "→",
+	["<left>"] = "←",
 }
-M.cazan2 = {
-	["\n"] = "^J",
-	["\t"] = "↔",
-	["\x0d"] = "⏎",
-	["scl"] = "s",
-	["\x80kb"] = "⌫",
-	["\x80ku"] = "↑",
-	["\x80kd"] = "↓",
-	["\x80kr"] = "→",
-	["\x80kl"] = "←",
-}
+local function lpad(str, len, char)
+	local strlen = vim.api.nvim_strwidth(str)
+	local res = string.rep(char or " ", len - strlen) .. str
+	return res
+end
 function M.format_key(key)
-	local text = M.cazan2[key]
-	if text == nil then
-		text = key
+	local key = vim.fn.keytrans(key)
+	if #key > 1 then
+		key = string.lower(key)
 	end
-	return text
+	key = M.cazan[key] or key
+	--key = pad(key, 5)
+	return key
 end
 
-function M.add_key(key)
+M.last_keys = {}
+function M.add_key(key) 
+	key = M.format_key(key)
+	table.insert(M.last_keys, key)
+	if(#M.last_keys > 20) then
+		table.remove(M.last_keys, 1)
+	end
+end
+
+function M.render()
+	local separator = " "
+	local line = table.concat(M.last_keys, separator)
+	while vim.api.nvim_strwidth(line) > M.width do
+		table.remove(M.last_keys, 1)
+		line = table.concat(M.last_keys, separator)
+	end
+	line = lpad(line, M.width)
+	vim.api.nvim_buf_set_lines(M.buf, 0, 1, false, { line })
 	M.open_win()
-	M.append_buf(M.format_key(key))
+end
+
+function M.callback(key)
+	M.add_key(key)
+	M.render()
 end
 
 function M.setup(_)
+	if M.buf == nil or not vim.api.nvim_buf_is_valid(M.buf) then
+		M.create_buf()
+	end
 	M.namespace = vim.api.nvim_create_namespace('ScreenKeys')
-	vim.on_key(M.add_key, M.namespace)
-	local M.augroup = vim.api.nvim_create_augroup('ScreenKeys', {
+	vim.on_key(M.callback, M.namespace)
+	M.augroup = vim.api.nvim_create_augroup('ScreenKeys', {
 		clear = true,
 	})
+	vim.api.nvim_set_hl(0, 'ScreenKeys', { link = "SpecialKey" })
 	vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
 		group = M.augroup,
 		pattern = {'*'},
 		callback = function(ev)
-			M.clear_buf()
+			M.last_keys = { }
 			M.close_win()
 		end
 		})
@@ -147,7 +127,6 @@ end
 function M.stop()
 	-- TODO clear on_key function callback from namespace M.namespace
 	vim.api.nvim_del_augroup_by_id(M.augroup)
-end
 end
 
 return M
